15f94c607a74aa08c2d3914bb0794c76421e3548
Isaiah Odhner <isaiahodhner@gmail.com>
2013-03-17 14:06:54 -0700

    M 373.9 433 L 127.1 433 L 2.5 216 L 127.1 1.4 L 373.9 1.4 L 498.5 216 L 373.9 433 L 373.9 433

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
+
+//We shouldn't need to munipulate the DOM except through raphael.js
 //function $(q){return document.querySelector(q);}
 
-//var paper=Raphael($("#ralphio"),"100%","100%");
-//var paper=Raphael(document.getElementById("r"),"100%","100%");
 var paper=Raphael(document.body,"100%","100%");
-//var paper=Raphael(0,0,500,500);
 
-//var test=paper.circle(50,50,15);
+//var circle=paper.circle(50,50,15);
+
+//It turns out hexagons aren't square grids.
+board=[];
+deck=[];
+
+//placeholder "board
+paper.path("M 373.9 433 L 127.1 433 L 2.5 216 L 127.1 1.4 L 373.9 1.4 L 498.5 216 L 373.9 433 L 373.9 433");
