50f3fd95196f3b2506b9a4861ac8713730b021fa
Isaiah Odhner <isaiahodhner@gmail.com>
2013-03-17 11:58:56 -0700

    Added basic frontend frame

---
+++ game.js
@@ -0,0 +1,8 @@
+//function $(q){return document.querySelector(q);}
+
+//var paper=Raphael($("#ralphio"),"100%","100%");
+//var paper=Raphael(document.getElementById("r"),"100%","100%");
+var paper=Raphael(document.body,"100%","100%");
+//var paper=Raphael(0,0,500,500);
+
+//var test=paper.circle(50,50,15);
